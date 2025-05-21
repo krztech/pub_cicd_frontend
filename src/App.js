21db2e912412b4290c8c755431794e79c667baa8
krztech <krztechdb@gmail.com>
2025-05-21 14:32:52 +0530

    center aligned content in app.js file

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App" style={{textAlign:"center"}}>
+    <div className="App" style={{textAlign:"center",margin:"50% auto"}}>
       <h2>Welcome to demo section on CICD pipeline</h2>
       <p>This particular server is running in AWS EC2 instance at South Pacific Mumbai region</p>
-      <p>powered by KRZ IT department</p>
+      <p style={{textAlign:'right'}}>from by KRZ IT department</p>
     </div>
   );
 }
