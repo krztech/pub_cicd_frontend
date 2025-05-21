ceddbe0ccf84a38161ca72d0c11c5801a9983213
krztech <krztechdb@gmail.com>
2025-05-21 14:47:14 +0530

    top aligned content in app.js file

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App" style={{textAlign:"center",margin:"50% auto"}}>
+    <div className="App" style={{textAlign:"center",margin:"0% auto"}}>
       <h2>Welcome to demo section on CICD pipeline</h2>
       <p>This particular server is running in AWS EC2 instance at South Pacific Mumbai region</p>
-      <p style={{textAlign:'right'}}>from by KRZ IT department</p>
+      <p style={{textAlign:'right'}}>from KRZ IT Team</p>
     </div>
   );
 }
