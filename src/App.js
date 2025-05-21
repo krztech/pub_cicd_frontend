716e89bd8f8904251915b1618ed15d3bafe70744
krztech <krztechdb@gmail.com>
2025-05-21 13:09:39 +0530

    altered App.js file

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
     <div className="App" style={{textAlign:"center"}}>
       <h2>Welcome to demo section on CICD pipeline</h2>
       <p>This particular server is running in AWS EC2 instance at South Pacific Mumbai region</p>
+      <p>from KRZ IT department</p>
     </div>
   );
 }
