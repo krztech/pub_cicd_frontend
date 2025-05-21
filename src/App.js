a0985da9e3446e557c27ba1bcb3ce16146f641cb
krztech <krztechdb@gmail.com>
2025-05-21 10:41:16 +0530

    made chage in app.js file by removing logo

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ function App() {
   return (
     <div className="App" style={{textAlign:"center"}}>
       <h2>Welcome to demo section on CICD pipeline</h2>
+      <p>This particular server is running in EC2 instance</p>
     </div>
   );
 }
