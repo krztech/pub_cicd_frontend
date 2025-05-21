afd1bad518ca43e21cd3ecf4c57257ef24706637
krztech <krztechdb@gmail.com>
2025-05-21 12:14:37 +0530

    made changes in app.js file

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App" style={{textAlign:"center"}}>
       <h2>Welcome to demo section on CICD pipeline</h2>
-      <p>This particular server is running in EC2 instance</p>
+      <p>This particular server is running in AWS EC2 instance at South Pacific Mumbai region</p>
     </div>
   );
 }
