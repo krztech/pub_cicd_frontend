90db1e95c06b837cf30b90e45ee6daf7309cba85
krztech <krztechdb@gmail.com>
2025-05-22 12:11:03 +0530

    worked on banner page css

--- src/Components/MainBanner.jsx
+++ src/Components/MainBanner.jsx
@@ -1,26 +1,18 @@
 import React from "react";
-import Navbar from "../Components/Navbar"
+import Navbar from "../../../Components/Navbar";
 
 const MainBanner = () => {
   return (
-    <div style={{ position: "relative", height: "856px" }}>
+    <div style={{ position: "relative", height: "856px", overflow: "hidden" }}>
       <Navbar />
-      <div
-        style={{
-          width: "933px",
-          height: "933px",
-          left: "292.42.px",
-          top: "1091.18px",
-          bottom: "-473.06px",
-          position: "absolute",
-        }}
-      ></div>
+
       <div
         style={{
           width: "493px",
           height: "128px",
-          top: "109px",
-          left:"30%",
+          top: "193px",
+          left: "50%",
+          transform: "translateX(-50%)",
           position: "absolute",
         }}
       >
@@ -36,10 +28,11 @@ const MainBanner = () => {
           left: "91px",
           bottom: "88px",
           position: "absolute",
+          textAlign: "left",
         }}
       >
-        <h4>Your Vision, Our Expertise</h4>
-        <p>
+        <h4 style={styles.subintrohead}>Your Vision, Our Expertise</h4>
+        <p style={styles.subintrotext}>
           Transforming data into insights, words into impact, designs into
           experiences, and concepts into reality—driven by innovation, powered
           by expertise.
@@ -52,14 +45,30 @@ const MainBanner = () => {
           right: "93px",
           bottom: "283px",
           position: "absolute",
+          textAlign: "right",
         }}
       >
-        <h4>Turning Challenges into Solutions</h4>
-        <p>
+        <h4 style={styles.subintrohead}>Turning Challenges into Solutions</h4>
+        <p style={styles.subintrotext}>
           We blend Data, Design, and Technology to craft impactful solutions
           tailored to your business needs.
         </p>
       </div>
+      <div
+        style={{
+          width: "933px",
+          height: "933px",
+          left: "50%",
+          transform: "translateX(-50%)",
+          bottom: "-503.06px",
+          borderRadius: "50%",
+          background: "linear-gradient(164.14deg, #FFFFFF 20.25%, #BBBBBB 77.11%)",
+          boxShadow: "0px 4px 60.8px 0px #FFFFFF inset",
+          position: "absolute",
+          zIndex: -1,
+          rotate: "-4.62deg",
+        }}
+      ></div>
     </div>
   );
 };
@@ -71,6 +80,7 @@ const styles = {
     lineHeight: "29px",
     textAlign: "center",
     verticalAlign: "middle",
+    fontFamily: "Manrope",
     color: "#FFFFF57",
   },
   introhead: {
@@ -78,7 +88,27 @@ const styles = {
     fontSize: "48px",
     lineHeight: "80px",
     textTransform: "uppercase",
+    fontFamily: "Lexend Zetta",
     color: "#545454",
+  },
+  subintrohead: {
+    fontFamily: "Lexend Deca",
+    fontWeight: 400,
+    fontSize: "24px",
+    lineHeight: "25px",
+    verticalAlign: "middle",
+    textTransform: "uppercase",
+    color: "#1B1464",
+  },
+  subintrotext: {
+    fontFamily: "Lexend Deca",
+    fontWeight: 300,
+    fontSize: "13px",
+    lineHeight: "17px",
+    verticalAlign: "middle",
+    textTransform: "capitalize",
+    color: "#242626",
+    marginTop: "10px",
   },
 };
 
