c4ac36c4c5d31c859a487356f89e2df7e5efa2bb
krztech <krztechdb@gmail.com>
2025-05-22 10:23:40 +0530

    worked on banner page

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Navbar from './Components/Navbar';
 function App() {
   return (
     <div className="App" style={{textAlign:"center",margin:"0% auto"}}>
-      <Navbar/>
       <MainBanner/>
     </div>
   );
